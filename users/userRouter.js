b4b842fcc53629f4ba05855a938a53ebf93bb99b
Dylan Collins <dcollins5590@students.mc3.edu>
2020-04-08 12:41:27 -0400

    finished MVP

--- users/userRouter.js
+++ users/userRouter.js
@@ -1,13 +1,24 @@
 const express = require('express');
 const router = express.Router();
 const dbUsers = require('./userDb');
+const dbPosts = require('../posts/postDb')
 
-router.post('/', (req, res) => {
-  // do your magic!
+router.post('/', validateUser,(req, res) => {
+  dbUsers.insert(req.body)
+  .then( user => {
+    res.status(201).json(user)
+  })
+  .catch(err => {
+    res.status(500).json({errorMessage: 'could not add the new user from database'})
+  })
 });
 
-router.post('/:id/posts', validateUserId, (req, res) => {
-  // do your magic!
+router.post('/:id/posts', validateUserId, validatePost,(req, res) => {
+  dbPosts.insert(req.body)
+  .then(post => {
+    res.status(200).json(post)
+  })
+  .catch(err => res.sendStatus(500).json({message: 'error when trying to insert the new post'}))
 });
 
 router.get('/', (req, res) => {
@@ -26,18 +37,50 @@ router.get('/:id', validateUserId,(req, res) => {
 });
 
 router.get('/:id/posts',validateUserId, (req, res) => {
-  // do your magic!
+  const id = req.params.id;
+  dbUsers.getUserPosts(id)
+  .then(post => {
+    res.status(200).json(post)
+  })
+  .catch(err => {
+    res.status(500).json({message: 'could not get post for database'})
+  })
 });
 
 router.delete('/:id',validateUserId, (req, res) => {
-  // do your magic!
+  const id = req.params.id;
+  dbUsers.remove(id)
+  .then(deletedUser => {
+    if(deletedUser !== 0){
+      res.status(200).json(req.user)
+    }else{
+      res.status(404).json({message: 'the user id was not deleted'})
+    }
+  })
+  .catch(err => {
+    res.status(500).json({message: 'the user was not delete because we could access the database'})
+  })
 });
 
-router.put('/:id', validateUserId,(req, res) => {
-  // do your magic!
+router.put('/:id', validateUserId, validateUser,(req, res) => {
+  const id = req.params.id;
+  const body = req.body;
+  dbUsers.update(id,body)
+  .then(updatedUser => {
+    if(updatedUser !== 0){
+      dbUsers.getById(id)
+      .then( user => {
+        res.status(200).json(user)
+      })
+      .catch(err => {
+        res.status(500).json({message: 'can not get user after being updated'})
+      })
+    }
+  })
+  .catch(err => {
+    res.status(500).json({message: 'can not update user'})
+  })
 });
-
-//custom middleware
 
 function validateUserId(req, res, next) {
   const id = req.params.id;
@@ -47,18 +90,28 @@ function validateUserId(req, res, next) {
       req.user = object;
       next()
     }else{
-      res.status(404).json({errorMessage: 'that user doesn not exist'})
+      res.status(404).json({errorMessage: 'invalid user id'})
     }
   })
   .catch(err => res.status(500).json({errorMessage: 'could not access the database'}))
 }
 
 function validateUser(req, res, next) {
-  // do your magic!
+  const body = req.body;
+  if(body.name){
+    next();
+  }else{
+    return res.status(400).json({ message: "missing user data"})
+  }
 }
 
 function validatePost(req, res, next) {
-  // do your magic!
+  const body = req.body;
+  if(body.text && body.user_id){
+    next()
+  }else{
+    return res.status(400).json({message: "missing required text field"})
+  }
 }
 
 module.exports = router;
