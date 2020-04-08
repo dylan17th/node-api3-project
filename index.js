7799b22ca63fb3cc1b38f02e7add73f68b65c7e3
Dylan Collins <dcollins5590@students.mc3.edu>
2020-04-08 10:52:52 -0400

    got the logger function to work

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// code away!
+const server = require('./server');
+const port = 8001;
+
+server.listen(port, ()=> {
+    console.log(`listening on port ${port}`)
+})
--- server.js
+++ server.js
@@ -2,12 +2,21 @@ const express = require('express');
 
 const server = express();
 
+server.use(express.json());
+server.use(logger);
+
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
 //custom middleware
 
-function logger(req, res, next) {}
+function logger(req, res, next) {
+  const method = req.method;
+  const url = req.originalUrl;
+  const time = new Date();
+  console.log(`${method}, ${url} , ${time}`)
+  next()
+}
 
 module.exports = server;
