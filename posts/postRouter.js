7e5362cf62cae134d4776819d29a3336f793fe5a
Dylan Collins <dcollins5590@students.mc3.edu>
2020-04-08 13:06:23 -0400

    finished a stretch goal

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -17,13 +17,36 @@ router.get('/:id',validatePostId, (req, res) => {
   res.status(200).json(post)
 });
 
-router.delete('/:id',validatePostId, (req, res) => {
-  // do your magic!
-
+router.delete('/:id',validatePostId, validatePost,(req, res) => {
+  const id = req.params.id;
+  dbPosts.remove(id)
+  .then(del => {
+    if(del !== 0 ){
+      res.status(200).json(req.post)
+    }else{
+      res.status(404).json({message: 'the post could not be deleted'})
+    }
+  })
+  .catch(err => {
+    res.status(500).json({message:'the post was not deleted by the database'})
+  })
 });
 
 router.put('/:id', validatePostId,(req, res) => {
-  // do your magic!
+  const id = req.params.id;
+  const body = req.body;
+  dbPosts.update(id, body)
+  .then(updatedPost => {
+    if(updatedPost == 1){
+        dbPosts.getById(id)
+        .then(object => {
+          res.status(200).json(object)
+        }).catch(err => res.status(500).json({message: 'post was updated but for some reason could not be retrieved from the database'}))
+    }else{
+      res.status(404).json({message: 'post was not updated'})
+    }
+  })
+  .catch(err => res.status(500).json({message : 'post was not updated in the database'}))
 });
 
 function validatePostId(req, res, next) {
@@ -39,6 +62,15 @@ function validatePostId(req, res, next) {
   })
   .catch(err => res.status(500).json({errorMessage: 'could not get user for database'}))
   
+}
+
+function validatePost(req,res,next){
+  const body = req.body;
+  if(body.text){
+    next()
+  }else{
+    res.status(400).json({message: 'you dont have a text in the body'})
+  }
 }
 
 module.exports = router;
--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const postRouter = require('./posts/postRouter');
 server.use(express.json());
 server.use(logger);
 server.use('/api/users', userRouter);
-server.use('/api/posts', postRouter)
+server.use('/api/posts', postRouter);
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
