f279e69ce1303ef8acb28b3525ca282f6f9ad646
Dylan Collins <dcollins5590@students.mc3.edu>
2020-04-08 11:32:54 -0400

    got some get request working and the middleware function validateUserId working

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -1,27 +1,44 @@
 const express = require('express');
-
 const router = express.Router();
+const dbPosts = require('./postDb')
 
 router.get('/', (req, res) => {
-  // do your magic!
+  dbPosts.get()
+  .then( post =>{
+    res.status(200).json(post)
+  })
+  .catch(err => {
+    res.status(500).json({messgae : `could'nt get posts for database`})
+  })
 });
 
-router.get('/:id', (req, res) => {
-  // do your magic!
+router.get('/:id',validatePostId, (req, res) => {
+  const post = req.post; 
+  res.status(200).json(post)
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id',validatePostId, (req, res) => {
   // do your magic!
+
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', validatePostId,(req, res) => {
   // do your magic!
 });
-
-// custom middleware
 
 function validatePostId(req, res, next) {
-  // do your magic!
+  const id = req.params.id;
+  dbPosts.getById(id)
+  .then( neededId => {
+    if(neededId){
+      req.post = neededId;
+      next()
+    }else{
+      res.status(404).json({errorMessage: "that user id does not exist"})
+    }
+  })
+  .catch(err => res.status(500).json({errorMessage: 'could not get user for database'}))
+  
 }
 
 module.exports = router;
--- users/userRouter.js
+++ users/userRouter.js
@@ -1,39 +1,56 @@
 const express = require('express');
-
 const router = express.Router();
+const dbUsers = require('./userDb');
 
 router.post('/', (req, res) => {
   // do your magic!
 });
 
-router.post('/:id/posts', (req, res) => {
+router.post('/:id/posts', validateUserId, (req, res) => {
   // do your magic!
 });
 
 router.get('/', (req, res) => {
+  dbUsers.get()
+  .then(users => {
+    res.status(200).json(users)
+  })
+  .catch(err => {
+    res.status(500).json({errorMessage: 'could not access database'})
+  })
+});
+
+router.get('/:id', validateUserId,(req, res) => {
+  const user = req.user;
+  res.status(200).json(user)
+});
+
+router.get('/:id/posts',validateUserId, (req, res) => {
   // do your magic!
 });
 
-router.get('/:id', (req, res) => {
+router.delete('/:id',validateUserId, (req, res) => {
   // do your magic!
 });
 
-router.get('/:id/posts', (req, res) => {
-  // do your magic!
-});
-
-router.delete('/:id', (req, res) => {
-  // do your magic!
-});
-
-router.put('/:id', (req, res) => {
+router.put('/:id', validateUserId,(req, res) => {
   // do your magic!
 });
 
 //custom middleware
 
 function validateUserId(req, res, next) {
-  // do your magic!
+  const id = req.params.id;
+  dbUsers.getById(id)
+  .then(object => {
+    if(object){
+      req.user = object;
+      next()
+    }else{
+      res.status(404).json({errorMessage: 'that user doesn not exist'})
+    }
+  })
+  .catch(err => res.status(500).json({errorMessage: 'could not access the database'}))
 }
 
 function validateUser(req, res, next) {
